f90a086f5460f915f4e8dbc6ba86cdffed17c2e2
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-10-17 15:53:26 +0300

    Add whitenoise package
    Add whitenoise package to the application for accessing static files

--- hc/settings.py
+++ hc/settings.py
@@ -132,6 +132,11 @@ STATICFILES_FINDERS = (
     'compressor.finders.CompressorFinder',
 )
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 COMPRESS_OFFLINE = True
 
 
