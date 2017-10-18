a801a723afde00a7e8100f61c3ea06ba34484257
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-10-18 13:00:07 +0300

    fix(settings): Separate Heroku Configuration
    Make separate configuration for Heroku deployment

--- hc/settings.py
+++ hc/settings.py
@@ -132,10 +132,9 @@ STATICFILES_FINDERS = (
     'compressor.finders.CompressorFinder',
 )
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
 
 COMPRESS_OFFLINE = True
 
