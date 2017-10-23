becfe69e2b55cf8599b87eb10c596ace75f2809d
Arnold Taremwa <taremwaarnold@gmail.com>
2017-10-23 12:36:06 +0300

    [chore #151948276] Readd comment instructions to test files.

--- hc/api/tests/test_check_model.py
+++ hc/api/tests/test_check_model.py
@@ -12,6 +12,7 @@ class CheckModelTestCase(TestCase):
 
         check.tags = " foo  bar "
         self.assertEquals(check.tags_list(), ["foo", "bar"])
+        ### Repeat above test for when check is an empty string 
 
         # test for when check is an empty string
         check.tags = ""
@@ -25,6 +26,8 @@ class CheckModelTestCase(TestCase):
 
         self.assertTrue(check.in_grace_period())
         self.assertEqual(check.get_status(), "up")
+        
+        ### The above 2 asserts fail. Make them pass 
 
     def test_paused_check_is_not_in_grace_period(self):
         check = Check()
@@ -35,3 +38,5 @@ class CheckModelTestCase(TestCase):
 
         check.status = "paused"
         self.assertFalse(check.in_grace_period())
+
+        ### Test that when a new check is created, it is not in the grace period 
