8b4b24bf575a2ad0c09b8dc696212eda2ee5098a
einsteincarrey <forensicslabnet@gmail.com>
2017-10-23 17:34:14 +0300

    [chore #151948276] Replace tabs with 4 spaces

--- hc/api/tests/test_admin.py
+++ hc/api/tests/test_admin.py
@@ -4,19 +4,19 @@ from hc.test import BaseTestCase
 
 class ApiAdminTestCase(BaseTestCase):
 
-	def setUp(self):
-		super(ApiAdminTestCase, self).setUp()
-		self.check = Check.objects.create(user=self.alice, tags="foo bar")
+    def setUp(self):
+        super(ApiAdminTestCase, self).setUp()
+        self.check = Check.objects.create(user=self.alice, tags="foo bar")
 
-		### Set Alice to be staff and superuser and save her :)
-		self.alice.is_staff = True
-		self.alice.is_superuser = True
+        ### Set Alice to be staff and superuser and save her :)
+        self.alice.is_staff = True
+        self.alice.is_superuser = True
 
-	def test_it_shows_channel_list_with_pushbullet(self):
-		self.client.login(username="alice@example.org", password="password")
+    def test_it_shows_channel_list_with_pushbullet(self):
+        self.client.login(username="alice@example.org", password="password")
 
-		ch = Channel(user=self.alice, kind="pushbullet", value="test-token")
-		ch.save()
+        ch = Channel(user=self.alice, kind="pushbullet", value="test-token")
+        ch.save()
 
-		### Assert for the push bullet
-		self.assertEqual(ch.kind, "pushbullet")
+        ### Assert for the push bullet
+        self.assertEqual(ch.kind, "pushbullet")
