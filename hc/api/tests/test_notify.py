c136cfb10e2425249f8ba12c0b5668ea50096349
Arnold Taremwa <taremwaarnold@gmail.com>
2017-10-24 08:55:33 +0300

    [Chore #151948276] Remove unused import

--- hc/api/tests/test_notify.py
+++ hc/api/tests/test_notify.py
@@ -4,7 +4,6 @@ from django.core import mail
 from django.test import override_settings
 from hc.api.models import Channel, Check, Notification
 from hc.test import BaseTestCase
-import mock
 from mock import patch
 from requests.exceptions import ConnectionError, Timeout
 
