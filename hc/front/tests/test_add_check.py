1268b301ecb6fef581f50a5ee6e3037f02a0e443
Macharia Marigi <macharia.marigi@andela.com>
2017-10-24 08:55:39 +0300

    Write tests for team access for checks

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -11,4 +11,12 @@ class AddCheckTestCase(BaseTestCase):
         self.assertRedirects(r, "/checks/")
         assert Check.objects.count() == 1
 
-    ### Test that team access works
+    def test_team_access(self):
+        url = "/checks/add/"
+        self.client.login(username="alice@example.org", password="password")
+        self.client.post(url)
+        check_code = Check.objects.filter(user=self.alice).first().code
+        self.client.logout()
+        self.client.login(username="bob@example.org", password="password")
+        r = self.client.get('/checks/')
+        self.assertIn(str(check_code), str(r.content))
