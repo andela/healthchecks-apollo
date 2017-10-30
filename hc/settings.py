a0b5116a39df03c81ba00bfb47eef66354df5241
Macharia Marigi <macharia.marigi@andela.com>
2017-10-30 14:25:57 +0300

    [CHORE]Get SITE_ROOT configuration from the environment

--- hc/settings.py
+++ hc/settings.py
@@ -91,7 +91,7 @@ DATABASES = {}
 
 if os.environ.get("Heroku") == 'TRUE':
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-    SITE_ROOT = "https://healthchecks-apollo.herokuapp.com"
+
 
 if os.environ.get("TRAVIS") == "TRUE":
     DATABASES = {
@@ -118,7 +118,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-SITE_ROOT = "http://localhost:8000"
+SITE_ROOT = os.getenv('SITE_ROOT')
 PING_ENDPOINT = SITE_ROOT + "/ping/"
 PING_EMAIL_DOMAIN = HOST
 STATIC_URL = '/static/'
