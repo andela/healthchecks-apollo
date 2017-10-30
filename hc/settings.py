f9a49601bcbcd6bbc6018b1a957dda612321aa2b
Macharia Marigi <macharia.marigi@andela.com>
2017-10-30 14:25:57 +0300

    [CHORE]Get SITE_ROOT configuration from the environment

--- hc/settings.py
+++ hc/settings.py
@@ -89,7 +89,7 @@ DATABASES = {}
 
 if os.environ.get("Heroku") == 'TRUE':
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-    SITE_ROOT = "https://healthchecks-apollo.herokuapp.com"
+
 
 
 
