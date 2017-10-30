5c494e780a2a2e6440d6497557f40ccaf44d5b45
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-10-30 12:35:45 +0300

    chore(front tests): Add tests for unresolved issues
    - Add tests for the functionality of unresolved issues
    
    [Fixes #151948282]

---
+++ hc/front/tests/test_unresolved_issues.py
@@ -0,0 +1,27 @@
+from hc.api.models import Check
+from hc.test import BaseTestCase
+from datetime import timedelta as td
+from django.utils import timezone
+
+
+class UnresolvedIssuesTestCase(BaseTestCase):
+
+    def setUp(self):
+        super(UnresolvedIssuesTestCase, self).setUp()
+        self.check = Check(user=self.alice, name="Alice Was Here")
+        self.check.save()
+
+    def test_unresolved_issues(self):
+        self.check.last_ping = timezone.now() - td(days=3)
+        self.check.status = "up"
+        self.check.save()
+
+        self.client.login(username="alice@example.org", password="password")
+        r = self.client.get("/unresolved/")
+
+        # Desktop
+        self.assertContains(r, "icon-down")
+
+        # Mobile
+        self.assertContains(r, "label-danger")
+        
