e95c33821ebc77cf75834f4e054bd5416a60233f
kasulani <kasulani@gmail.com>
2017-10-31 11:40:01 +0300

    [Feature #151948280] refactors profile view

--- hc/accounts/views.py
+++ hc/accounts/views.py
@@ -153,12 +153,18 @@ def profile(request):
             messages.info(request, "The API key has been revoked!")
         elif "show_api_key" in request.POST:
             show_api_key = True
+        elif "update_reports_allowed" in request.POST:
+            form = ReportSettingsForm(request.POST)
+            if form.is_valid():
+                profile.reports_allowed = form.cleaned_data["reports_allowed"]
+                profile.save()
+                messages.success(request, "Your settings have been updated!")
         elif "update_reports_period" in request.POST:
             form = ReportSettingsForm(request.POST)
             if form.is_valid():
                 profile.report_period = form.cleaned_data["report_period"]
                 profile.save()
-                messages.success(request, "Your settings have been updated!")
+                messages.success(request, "Your settings have been updated! Your reporting period has been changed")
         elif "invite_team_member" in request.POST:
             if not profile.team_access_allowed:
                 return HttpResponseForbidden()
