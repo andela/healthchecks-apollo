afe59c8722fd988e2580fee8cd0266322da6c6ff
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-11-09 09:43:04 +0300

    test(notify too often): Add tests for notify too often
    - Add unittests for the feature of notifying when a check is run too often
    
    [Finishes #152588131]

---
+++ hc/api/tests/test_notify_too_often.py
@@ -0,0 +1,37 @@
+from django.test import Client, TestCase
+
+from hc.api.models import Check, Notification, Ping
+
+class FrequentPingTestCase(TestCase):
+
+    def setUp(self):
+        super(FrequentPingTestCase, self).setUp()
+        self.check = Check.objects.create()
+
+    def test_check_status(self):
+        r = self.client.get("/ping/%s/" % self.check.code)
+        self.assertEqual(200, r.status_code)
+
+        self.check.refresh_from_db()
+        self.assertEqual("up", self.check.status)
+
+        r_2 = self.client.get("/ping/%s/" % self.check.code)
+        self.assertEqual(200, r_2.status_code)
+
+        self.check.refresh_from_db()
+        self.assertEqual("early", self.check.status)
+
+    def test_sends_ping(self):
+        r = self.client.get("/ping/%s/" % self.check.code)
+        self.assertEqual(200, r.status_code)
+
+        self.check.refresh_from_db()
+        self.assertEqual("up", self.check.status)
+
+        r_2 = self.client.get("/ping/%s/" % self.check.code)
+        self.assertEqual(200, r_2.status_code)
+
+        ping = Ping.objects.latest("id")
+        self.assertEqual(self.check.id, ping.owner_id)
+
+        
