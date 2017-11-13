d7bf6b650de5ec924cc44ff57ec5165095e44755
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-11-13 16:49:41 +0300

    fix(views): Resolve merge conflicts
    - Resolve merge conflicts in the views file
    
    [Fixes #151948282]

--- hc/front/views.py
+++ hc/front/views.py
@@ -14,7 +14,7 @@ from django.utils import timezone
 from django.utils.crypto import get_random_string
 from django.utils.six.moves.urllib.parse import urlencode
 from hc.api.decorators import uuid_or_400
-from hc.api.models import DEFAULT_GRACE, DEFAULT_TIMEOUT, Channel, Check, Ping, Notification
+from hc.api.models import DEFAULT_GRACE, DEFAULT_TIMEOUT, Channel, Check, Ping
 from hc.front.forms import (AddChannelForm, AddWebhookForm, NameTagsForm,
                             TimeoutForm)
 
